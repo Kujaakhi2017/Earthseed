12408536dc6c65b8a15d13454c620f092a5c6ac2
Freddy Haug <haug.freddy01@gmail.com>
2018-11-10 18:38:36 -0500

    upadted functions.c.

--- functions.c
+++ functions.c
@@ -37,6 +37,86 @@ void generatePlanetStats()
     return; 
 }
 //Stat
-//Choose System
+
+//Choose System 
 //Random Event
+
+//endGame
+void endGame()
+{
+    srand(time(0));
+    //initialize variables
+    bool forests = false, goodPlants = false, melt = false;
+    bool goodRuins= false, scarcity = false, ;
+    String wetness, atmosphere;
+    int fateValue=0, tempResult;
+
+    //Used to calculate the correct phrase for paragrapgh
+    if(planet_Temp<0)
+        tempResult =0;
+    else if(planet_Temp>=0 && planet_Temp<=500)
+        tempResult =1;
+    else 
+        tempResult =2;
+
+    //Calculate the forest conditions
+    if (planet_Water > 1 && planet_Water < 4)
+        forests = true;
+
+    //Random statement that determines good plants
+    //Good plants implies that there are good animals too
+    if(rand()%2 == 1)
+        goodPlants = true;
+
+    //Calculates the ruins conditions
+    if(rand()%2 == 1)
+        goodRuins = true;
+
+    //Scarcity 
+    if(tempResult == 0 || tempResult == 4 || planet_Water == 0 || planet_Water == 4)
+        scarcity = true; 
+
+    //Calculate if planet is wet or dry
+    if(planet_Water >= 2)
+        wetness = "wet";
+    else wetness = "dry";
+
+    //Calculate fate of colonists upon landing
+    if(ship_Landing < 70)
+        fateValue = rand() % 100;
+
+    ship_Colonists = ship_Colonists - fateValue;
+
+    //Calculate planet atmosphere
+    if(planet_Atmosphere == 0)
+        atmosphere = "very thin";
+    else if(planet_Atmosphere == 1)
+        atmosphere = "thin";
+    else if(planet_Atmosphere == 2)
+        atmosphere = "Earth-like";
+    else if(planet_Atmosphere == 3)
+        atmosphere = "toxic";
+    else 
+        atmosphere = "corrosive";
+
+    //Temperature calculation
+    if(planet_Temp >500)
+        melt = true;
+
+    //to see if we can live on a planet we look at 
+    //construction, resoucrses, landing
+
+//-------------
+    //Print out the first paragraph
+    Serial.print("The Earthseed entered into the planet’s %s atmosphere", atmosphere);
+    Serial.print("and activated its landing gear as soon as it approached the planet’s %s surface.", wetness);
+    if(fateValue == 0)
+        
+
+    //Calculate the variables
+    //Consider the variables
+    //Display correct messages
+
+}
+
 //New Game
