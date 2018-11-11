2c0975dd91e663c974b6345ab94fd362fc283b22
Akash Kumar <34189485+Kujaakhi2017@users.noreply.github.com>
2018-11-10 23:12:36 -0500

    Update zzMainEnd.ino

--- zzMainEnd.ino
+++ zzMainEnd.ino
@@ -1,1 +1,22 @@
+// Used to print the final results of the game.
 
+void showPlanetStats() {
+}
+
+void showShipStats() {
+}
+
+void showCivilization() {
+}
+
+void showScore(int colonists, int scientific, int cultural, int resources, int civilization) {
+    int score = colonists + scientific + cultural;
+    score += scientific*100;
+    score += cultural*100;
+    score += resources*100;
+    score += civilization*100;
+
+    lcd.setCursor(0,0);
+    String output = "Score: " + score;
+    lcd.print(output);
+}
